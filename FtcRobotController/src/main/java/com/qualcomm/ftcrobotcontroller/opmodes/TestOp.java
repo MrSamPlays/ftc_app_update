11e9c0500954b13f00e2aea4af9e403da0969a73
SamPhilip <samuel.philip@gmail.com>
2016-04-26 19:12:25 -0400

    Initial commit

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TestOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TestOp.java
@@ -1,48 +1,74 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
-import android.content.Context;
-import android.hardware.*;
-
+import com.qualcomm.ftcrobotcontroller.drivers.TouchSensor;
+import com.qualcomm.hardware.hitechnic.HiTechnicNxtColorSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.LegacyModule;
 
-import java.util.List;
-import java.util.jar.JarFile;
 
 /**
  * Created by sam on 15-Mar-16.
  */
 
 public class TestOp extends LinearOpMode {
+    private static LegacyModule legMod;
+    boolean LEDisEnabled = false;
     private DcMotor BL;
     private DcMotor BR;
     private LegacyModule l;
     private ColorSensor c;
-    boolean LEDisEnabled = false;
+    private HiTechnicNxtColorSensor e;
+
+    public static LegacyModule getLegMod() {
+        return legMod;
+    }
+
     void initialize() {
         BL = hardwareMap.dcMotor.get("m1");
         BR = hardwareMap.dcMotor.get("m2");
         c = hardwareMap.colorSensor.get("colour");
-        l = hardwareMap.legacyModule.get("Legacy Module 1");
+        legMod = hardwareMap.legacyModule.get("Legacy Module 1");
+        e = new HiTechnicNxtColorSensor(legMod, 0);
         BL.setDirection(DcMotor.Direction.REVERSE);
         BL.setMode(DcMotorController.RunMode.RESET_ENCODERS);
         BR.setMode(DcMotorController.RunMode.RESET_ENCODERS);
         BL.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
         BR.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
-        l.enableAnalogReadMode(0);
+        legMod.enableAnalogReadMode(0);
+
+        legMod.enable9v(5, true);
+        legMod.enableAnalogReadMode(5);
+
+        TouchSensor.init();
     }
+
+    double scaleInput(double d) {
+        double[] br = {0, 0.1, 0.2, 0.2, 0.2, 0.5, 0.7, 0.7, 0.9, 1, 1};
+        int index = (int) Math.round(d);
+        try {
+            if (index < 0) {
+                index = -index;
+                return -br[index];
+            }
+        } catch (ArrayIndexOutOfBoundsException a) {
+            a.printStackTrace();
+            a.getCause();
+        }
+        return br[index];
+    }
+
     @Override
     public void runOpMode() throws InterruptedException {
-
         initialize();
         waitForStart();
         while (opModeIsActive()) {
-
             BL.setPower(gamepad1.left_stick_y);
             BR.setPower(gamepad1.right_stick_y);
+            // BL.setPower(scaleInput(gamepad1.left_stick_y));
+            // BR.setPower(scaleInput(gamepad1.right_stick_y));
             if (gamepad1.a) {
                 BL.setMode(DcMotorController.RunMode.RESET_ENCODERS);
                 BR.setMode(DcMotorController.RunMode.RESET_ENCODERS);
@@ -59,8 +85,13 @@ public class TestOp extends LinearOpMode {
                 }
                 sleep(100);
             }
-            telemetry.addData("Encoders", BL.getCurrentPosition() + "\n" + BR.getCurrentPosition());
-            telemetry.addData("Colour sensor", l.readAnalog(0).toString() + "\n" + Integer.toHexString(c.argb()));
+
+            telemetry.addData("Touch", TouchSensor.getPressed());
+
+            telemetry.addData("Colour sensor0", legMod.readAnalog(5)[0] + "\n" + Integer.toHexString(c.argb()));
+            telemetry.addData("Colour sensor1", legMod.readAnalog(5)[1] + "\n" + Integer.toHexString(c.argb()));
+
+            telemetry.addData("HtColorSensor", "#" + Integer.toHexString(e.argb()));
         }
     }
 }
